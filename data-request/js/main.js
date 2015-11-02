0fb57dc4c788052ab0beef2054f72204cd08a9ea
iGuest <kailock@uw.edu>
2015-11-02 14:14:44 -0800

    lab work whoo

--- data-request/js/main.js
+++ data-request/js/main.js
@@ -1,4 +1,17 @@
 // Create application
 
+var myApp = angular.module('myApp', [])
 
 // Bind controller customersCtrl
+myApp.controller('customerCtrl', function($scope, $http){
+    $http.get('http://www.w3schools.com/angular/customers.php').then(function(response){
+        $scope.names = response.data.records
+    })
+
+    // $http({
+    //     method: 'GET',
+    //     url: 'http://www.w3schools.com/angular/customers.php'
+    // }).then(function successCallback(response) {
+    //     $scope.names = response.names
+    // });
+})
