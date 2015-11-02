35b49b09396c4e22153556afe7fc9289f8b605ea
iGuest <kailock@uw.edu>
2015-11-02 13:48:42 -0800

    lab work

--- mad-libs/js/main.js
+++ mad-libs/js/main.js
@@ -1,8 +1,15 @@
 // Create application
 
+var myApp = angular.module('myApp', [])
+
 
 // Bind controller madCtrl
-
+myApp.controller('myCtrl', function($scope){
+    $scope.name = "Kaitlin"
+    $scope.age = "20"
+    $scope.location = "Mars"
+    $scope.food = "fried chicken"
+})
 
 	// Set initial values for name, age, location, and food
 
